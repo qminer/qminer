a8195e00a8dfe7907110a85e0a82000bc192f6db
blazf <blaz@blazfortuna.com>
2014-03-18 15:45:21 +0100

    Cleaner testing reports

--- src/qminer/js/assert.js
+++ src/qminer/js/assert.js
@@ -1,33 +1,34 @@
 exports.silent = false;
+exports.consoleTitle = "Test";
 
 // prints success if it comes to this
 exports.run = function(test, msg) {
-    if (!this.silent) { console.say("Test", "PASS(ok): " + msg); }
+    if (!this.silent) { console.say(this.consoleTitle, "PASS(ok): " + msg); }
 }
 
 // checks if condition is true
 exports.ok = function(test, msg) {
     if (test) {
-        if (!this.silent) { console.say("Test", "PASS(ok): " + msg); }
+        if (!this.silent) { console.say(this.consoleTitle, "PASS(ok): " + msg); }
     } else {
-        console.say("Test", "FAIL(ok): " + msg);
+        console.say(this.consoleTitle, "FAIL(ok): " + msg);
     }
 }
 
 // checks if object exists (!=null)
 exports.exists = function(obj, msg) {
     if (obj != null) {
-        if (!this.silent) { console.say("Test", "PASS(exists): " + msg); }
+        if (!this.silent) { console.say(this.consoleTitle, "PASS(exists): " + msg); }
     } else {
-        console.say("Test", "FAIL(exists): " + msg);
+        console.say(this.consoleTitle, "FAIL(exists): " + msg);
     }
 }
 
 // checks if condition is true
 exports.equals = function(test, ok, msg) {
     if (test == ok) {
-        if (!this.silent) { console.say("Test", "PASS(equals:" + test + "=" + ok + "): " + msg); }
+        if (!this.silent) { console.say(this.consoleTitle, "PASS(equals:" + test + "=" + ok + "): " + msg); }
     } else {
-        console.say("Test", "FAIL(equals:" + test + "=" + ok + "): " + msg);
+        console.say(this.consoleTitle, "FAIL(equals:" + test + "=" + ok + "): " + msg);
     }
 }
