ed67a767ff2fa531347f6e00c1d9543543cf6960
Jan Rupnik <jan.rupnik@ijs.si>
2014-06-01 10:44:42 +0200

    libuv build fix

--- src/third_party/libuv-build.bat
+++ src/third_party/libuv-build.bat
@@ -1,5 +1,5 @@
-"c:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" /build "Debug|x64" libuv\uv.sln
-"c:\Program Files (x86)\Microsoft Visual Studio 10.0\Common7\IDE\devenv.com" /build "Release|x64" libuv\uv.sln
+"c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" /build "Debug|x64" libuv\uv.sln
+"c:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.com" /build "Release|x64" libuv\uv.sln
 
 copy /y libuv\x64\Release\libuv.lib
 
