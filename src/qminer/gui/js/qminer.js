da7d3a2f23a9a9f139c7ada175b044291d563ed5
Blaz Fortuna <blaz@blazfortuna.com>
2014-06-10 14:00:20 +0200

    Update qminer.js

--- src/qminer/gui/js/qminer.js
+++ src/qminer/gui/js/qminer.js
@@ -1,13 +1,8 @@
-function StoreCtrl($scope) {
+function StoreCtrl($scope, $http) {
     $scope.stores = [ ];
     
     // get list of stores from qminer
-    $.ajax({
-        url: '../qm_stores',
-        success: function (data) { 
-            angular.forEach(data, function (store) {
-                $scope.stores.push(store)
-            });
-        }
-    });    
+    $http.get('../qm_stores').success(function (data) {
+        $scope.stores = data;
+    });
 }
