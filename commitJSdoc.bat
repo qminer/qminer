371c1d707cdd5fa2fe8afd2ce772f54bc4b02a82
Jan Rupnik <jan.rupnik@ijs.si>
2014-06-12 17:23:50 +0200

    dir function updated

--- commitJSdoc.bat
+++ commitJSdoc.bat
@@ -1,13 +1,12 @@
 bash genJSdoc.sh
-cp docjs/jsdocfinal.txt ../qminer.wiki/JavaScript.mdconsole after bat file finish stay in folder
+cp docjs/jsdocfinal.txt ../qminer.wiki/JavaScript.md
 cd ..
 cd qminer.wiki
-git add JavaScript.md
-git commit --message=AutoDoc
-
 
 @rem Do not use "echo off" to not affect any child calls.
 @setlocal
+git add JavaScript.md
+git commit --message=AutoDoc
 
 @rem Get the absolute path to the current directory, which is assumed to be the
 @rem Git installation root.
@@ -19,6 +18,7 @@ git commit --message=AutoDoc
 
 @set PLINK_PROTOCOL=ssh
 @if not defined TERM set TERM=msys
+git pull https://github.com/qminer/qminer.wiki.git
 git push
 @endlocal
 cd ..\qminer
