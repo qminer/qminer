6c7ce22c680a8d4dbc3cd32583052c3d4e44cbe4
Gregor Leban <gleban@gmail.com>
2014-06-23 17:43:12 +0200

    TFileStackWalker for saving stacks to error file

--- src/glib/base/ut.cpp
+++ src/glib/base/ut.cpp
@@ -130,6 +130,91 @@ void TLogNotify::OnStatus(const TStr& MsgStr) {
 		MsgStr.CStr()));
 }
 
+//////////////////////////////////////
+// File-Notify
+TFileNotify::TFileNotify(const TStr& _FileName, bool _AddTimeStamp, bool _SeparateFilesForEachDay) 
+	: FileName(_FileName), AddTimeStamp(_AddTimeStamp), SeparateFilesForEachDay(_SeparateFilesForEachDay) { 
+	if (! SeparateFilesForEachDay)
+		File = TFOut::New(_FileName, true);
+	LastLogDate = "";
+}
+
+void TFileNotify::OpenNewFileForDate()
+{
+	LastLogDate = TTm::GetCurLocTm().GetWebLogDateStr();
+	FileName.ChangeChAll('\\', '/');
+	TStr Path, FName; FileName.SplitOnLastCh(Path, '/', FName);
+	File = TFOut::New(Path + "/" + LastLogDate + " " + FName, true);
+}
+
+void TFileNotify::OnStatus(const TStr& MsgStr) {
+	if (SeparateFilesForEachDay && TTm::GetCurLocTm().GetWebLogDateStr() != LastLogDate)
+		OpenNewFileForDate();
+	if (AddTimeStamp) {
+		TTm NowTm = TTm::GetCurLocTm();
+		if (SeparateFilesForEachDay) {
+			File->PutStrFmt("[%s] ", NowTm.GetHMSTColonDotStr(true, false).CStr());
+			File->PutStrLn(MsgStr);
+		}
+		else {
+			File->PutStrFmt("[%s %s] ",
+				NowTm.GetYMDDashStr().CStr(),
+				NowTm.GetHMSTColonDotStr(true, false).CStr());
+			File->PutStrLn(MsgStr);
+		}
+	}
+	else
+		File->PutStrLn(MsgStr);
+	File->Flush();
+}
+
+void TFileNotify::OnNotify(const TNotifyType& Type, const TStr& MsgStr){
+	if (SeparateFilesForEachDay && TTm::GetCurLocTm().GetWebLogDateStr() != LastLogDate)
+		OpenNewFileForDate();
+	TStr TypeStr = "";
+	if (Type==ntInfo) TypeStr = "INFO";
+	else if (Type == ntErr) TypeStr = "ERROR";
+	else if (Type == ntWarn) TypeStr = "WARNING";
+	else if (Type == ntStat) TypeStr = "STAT";
+
+	if (AddTimeStamp) {
+		TTm NowTm = TTm::GetCurLocTm();
+		if (SeparateFilesForEachDay) 
+			File->PutStrFmt("[%s] %s: %s\n", 
+				NowTm.GetHMSTColonDotStr(true, false).CStr(), 
+				TypeStr.CStr(),
+				MsgStr.CStr());
+		else
+			File->PutStrFmt("[%s %s] %s: %s\n", 
+				NowTm.GetYMDDashStr().CStr(), 
+				NowTm.GetHMSTColonDotStr(true, false).CStr(), 
+				TypeStr.CStr(),
+				MsgStr.CStr());
+	}
+	else 
+		File->PutStrFmt("%s: %s\n", TypeStr.CStr(), MsgStr.CStr());
+	File->Flush();
+}
+
+void TStrNotify::OnNotify(const TNotifyType& Type, const TStr& MsgStr)
+{
+	if (Type == ntInfo) {
+		Log += MsgStr + "\n";
+	}
+	else {
+		TStr TypeStr = TNotify::GetTypeStr(Type, false);
+		Log += TypeStr + " " + MsgStr + "\n";
+	}
+}
+
+void TStrNotify::OnStatus(const TStr& MsgStr)
+{
+	Log += MsgStr;
+	// print '\n' if message not overlayed
+	if ((!MsgStr.Empty()) && (MsgStr.LastCh() != '\r'))
+		Log += "\n";
+}
+
 /////////////////////////////////////////////////
 // Exception
 TExcept::TOnExceptF TExcept::OnExceptF=NULL;
