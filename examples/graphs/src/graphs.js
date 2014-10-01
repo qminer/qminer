a5685d65639629ec3373cb3c6e11acb22b6e1cc1
Mario Karlovcec <mario.karlovcec@gmail.com>
2014-10-01 13:51:40 +0200

    added graph.eachNode() and graph.eachEdge()

--- examples/graphs/src/graphs.js
+++ examples/graphs/src/graphs.js
@@ -27,21 +27,35 @@ g1.addEdge(1, 3); g2.addEdge(1, 3); g3.addEdge(1, 3);
 g1.addEdge(1, 4); g2.addEdge(1, 4); g3.addEdge(1, 4);
 
 // iterating graph and returning node ids, degree and degree centrality
+
+// iteration can be executed using eachNode and eachEdge
+console.log("UNDIRECTED GRAPH (g1):");
+g1.eachNode(function (N) { console.log("id: " + N.id + " deg: " + N.deg); });
+g1.eachEdge(function (E) { console.log("n1: " + E.srcId + " n2: " + E.dstId); });
+
+console.log("DIRECTED GRAPH (g2):");
+g2.eachNode(function (N) { console.log("id: " + N.id + " deg: " + N.deg); });
+g2.eachEdge(function (E) { console.log("n1: " + E.srcId + " n2: " + E.dstId); });
+
+console.log("DIRECTED-MULTIGRAPH (g3):");
+g3.eachNode(function (N) { console.log("id: " + N.id + " deg: " + N.deg); });
+g3.eachEdge(function (E) { console.log("n1: " + E.srcId + " n2: " + E.dstId); });
+
 var br = 0;
-console.say("UNDIRECTED GRAPH:");
-for (var i = g1.firstNode ; br < g1.nodes; i.next) {
+console.say("UNDIRECTED GRAPH (g1):");
+for (var i = g1.firstNode ; br < g1.nodes; i.next()) {
     console.log("id: " + i.id + ", deg: " + i.deg + ", inDeg: " + i.inDeg + ", outDeg: " + i.outDeg);
     br++;
 }
 br = 0;
-console.say("DIRECTED GRAPH:");
-for (var i = g2.firstNode ; br < g2.nodes; i.next) {
+console.say("DIRECTED GRAPH (g2):");
+for (var i = g2.firstNode ; br < g2.nodes; i.next()) {
     console.log("id: " + i.id + ", deg: " + i.deg + ", inDeg: " + i.inDeg + ", outDeg: " + i.outDeg);
     br++;
 }
 br = 0;
-console.say("DIRECTED-MULTIGRAPH:");
-for (var i = g3.firstNode ; br < g3.nodes; i.next) {
+console.say("DIRECTED-MULTIGRAPH (g3):");
+for (var i = g3.firstNode ; br < g3.nodes; i.next()) {
     console.log("id: " + i.id + ", deg: " + i.deg + ", inDeg: " + i.inDeg + ", outDeg: " + i.outDeg);
     br++;
 }
@@ -65,7 +79,7 @@ graphs.push(g2005); graphs.push(g2006);
 // determining communities for the array of graphs and storing the results in array of sparse vectors
 var communities = new Array();
 for (var i = 0; i < graphs.length; i++) {
-    communities.push(snap.CommunityDetection(graphs[i], "gn"));
+    communities.push(snap.communityDetection(graphs[i], "gn"));
 }
 
 // return json string of graph evolution
@@ -82,9 +96,9 @@ var g =  snap.removeNodes(g, 3)
 
 // detect communities using 2 different algorithms
 console.log("Calculating communities using Clauset-Newman-Moore community detection method");
-var CmtyCNM = snap.CommunityDetection(g, "cnm");
+var CmtyCNM = snap.communityDetection(g, "cnm");
 console.log("Calculating communities using Info-map community detection method");
-var CmtyImap = snap.CommunityDetection(g, "imap");
+var CmtyImap = snap.communityDetection(g, "imap");
 
 // draw the graph using two different colorings
 
