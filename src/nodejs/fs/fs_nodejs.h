ce5b090dae107e328e80b133cdec8a9a7c7f1b8a
lstopar <luka.stopar@ijs.si>
2014-12-22 12:29:22 +0100

    fix

--- src/nodejs/fs/fs_nodejs.h
+++ src/nodejs/fs/fs_nodejs.h
@@ -1,7 +1,9 @@
 #ifndef QMINER_FS_NODEJS
 #define QMINER_FS_NODEJS
 
-#define BUILDING_NODE_EXTENSION
+#ifndef BUILDING_NODE_EXTENSION
+	#define BUILDING_NODE_EXTENSION
+#endif
 
 #include <node.h>
 #include <node_object_wrap.h>
