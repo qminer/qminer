bf2db558f937229a697c06b250361a65b2c5716a
bkazic <blaz.kazic@ijs.si>
2015-01-09 13:45:22 +0100

    Added std function to stat module (node) Added zscore function to stat module (node) Added mocha unit test for module stat (node)

--- src/qminer/nodejs/stat/stat_nodejs.cpp
+++ src/qminer/nodejs/stat/stat_nodejs.cpp
@@ -1,14 +1,13 @@
 #include "stat_nodejs.h"
 
 ///////////////////////////////
-// QMiner-Stat
+// NodeJs-QMiner-Stat
 
 void TNodeJsStat::Init(v8::Handle<v8::Object> exports) {
 	v8::Isolate* Isolate = v8::Isolate::GetCurrent();
 	v8::HandleScope HandleScope(Isolate);
 
 	// Add all methods, getters and setters here.
-	//NODE_SET_METHOD(exports, "svd", svd);
 	NODE_SET_METHOD(exports, "mean", _mean);
 	NODE_SET_METHOD(exports, "std", _std);
 	NODE_SET_METHOD(exports, "zscore", _zscore);
@@ -17,45 +16,83 @@ void TNodeJsStat::Init(v8::Handle<v8::Object> exports) {
 void TNodeJsStat::mean(const v8::FunctionCallbackInfo<v8::Value>& Args) {
 	v8::Isolate* Isolate = v8::Isolate::GetCurrent();
 	v8::HandleScope HandleScope(Isolate);
+	
+	EAssertR(Args.Length() != 0, "Error using stat.mean function. Not enough input arguments.");
+	EAssertR(Args[0]->IsObject() && (TNodeJsUtil::IsClass(Args[0]->ToObject(), "TFltV") || TNodeJsUtil::IsClass(Args[0]->ToObject(), "TFltVV")), 
+		"Error using stat.std function. First argument should be la.vector or  la.matrix.");
+	
 	// Dim parameter
 	double Dim = TNodeJsUtil::GetArgFlt(Args, 1, 1); // Default dim is 1
 
 	if (TNodeJsUtil::IsArgClass(Args, 0, "TFltV")) {
-		printf("Im in TFltV!!");
-		EAssertR(Args[0]->IsObject() && TNodeJsUtil::IsClass(Args[0]->ToObject(), "TFltV"), "TNodeJsStore constructor expecting store name and base object as arguments");
 		// If input argument is vec
-		//TNodeJsVec* Test = ObjectWrap::Unwrap<TNodeJsVec>(Args[0]->ToObject()); // Doesent work
+		//TNodeJsVec* Test = ObjectWrap::Unwrap<TNodeJsVec>(Args[0]->ToObject()); 
 		TNodeJsVec<TFlt, TAuxFltV>* JsVec = ObjectWrap::Unwrap< TNodeJsVec< TFlt, TAuxFltV > >(Args[0]->ToObject());
 		Args.GetReturnValue().Set(v8::Number::New(Isolate, TLAMisc::Mean(JsVec->Vec)));
 		return;
 	}
 	if (TNodeJsUtil::IsArgClass(Args, 0, "TFltVV")) {
-		printf("Im in TFltVV!!");
-		EAssertR(Args[0]->IsObject() && TNodeJsUtil::IsClass(Args[0]->ToObject(), "TFltVV"), "TNodeJsStore constructor expecting store name and base object as arguments");
-		 //If input argument is matrix
+		//If input argument is matrix
 		TFltV Vec;
 		TNodeJsFltVV* JsMat = ObjectWrap::Unwrap<TNodeJsFltVV>(Args[0]->ToObject());
 		TLAMisc::Mean(JsMat->Mat, Vec, Dim);
 		Args.GetReturnValue().Set(TNodeJsVec<TFlt, TAuxFltV>::New(Vec));
 		return;
 	}
-	printf("Im at the end!!");
-	//throw TExcept::New("la.mean() can take only matrix, or vector as first input argument.");
-	EAssertR(false, "la.mean() can take only matrix, or vector as first input argument.");
 }
 
 void TNodeJsStat::std(const v8::FunctionCallbackInfo<v8::Value>& Args) {
 	v8::Isolate* Isolate = v8::Isolate::GetCurrent();
 	v8::HandleScope HandleScope(Isolate);
+	
+	EAssertR(Args.Length() != 0, "Error using stat.std function. Not enough input arguments.");
+	EAssertR(Args[0]->IsObject() && (TNodeJsUtil::IsClass(Args[0]->ToObject(), "TFltV") || TNodeJsUtil::IsClass(Args[0]->ToObject(), "TFltVV")),
+		"Error using stat.std function. First argument should be la.vector or la.matrix.");
 
-	//Args.GetReturnValue().Set(v8::Number::New(Isolate, Sum));
+	double Flag = TNodeJsUtil::GetArgFlt(Args, 1, 0); // Default flag is 0
+	double Dim = TNodeJsUtil::GetArgFlt(Args, 2, 1); // Default dim is 1
+
+	if (TNodeJsUtil::IsArgClass(Args, 0, "TFltV")) {
+		// If input argument is vec
+		// TODO
+		throw TExcept::New("stat.std(vec) not implemented yet. Use stat.std(mat).");
+	}
+	if (TNodeJsUtil::IsArgClass(Args, 0, "TFltVV")) {
+		//If input argument is matrix
+		TNodeJsFltVV* JsMat = ObjectWrap::Unwrap<TNodeJsFltVV>(Args[0]->ToObject());
+		TFltV Res;
+		TLAMisc::Std(JsMat->Mat, Res, Flag, Dim);
+
+		Args.GetReturnValue().Set(TNodeJsVec<TFlt, TAuxFltV>::New(Res));
+		return;
+	}
 }
 
 void TNodeJsStat::zscore(const v8::FunctionCallbackInfo<v8::Value>& Args) {
 	v8::Isolate* Isolate = v8::Isolate::GetCurrent();
 	v8::HandleScope HandleScope(Isolate);
+	
+	EAssertR(Args.Length() != 0, "Error using stat.zscore function. Not enough input arguments.");
+	EAssertR(Args[0]->IsObject() && TNodeJsUtil::IsClass(Args[0]->ToObject(), "TFltVV"), "Error using stat.zscore function. First argument should be a matrix.");
 
-	//Args.GetReturnValue().Set(v8::Number::New(Isolate, Sum));
+	double Flag = TNodeJsUtil::GetArgFlt(Args, 1, 0); // Default flag is 0
+	double Dim = TNodeJsUtil::GetArgFlt(Args, 2, 1); // Default dim is 1
+
+	TNodeJsFltVV* JsMat = ObjectWrap::Unwrap<TNodeJsFltVV>(Args[0]->ToObject());
+	v8::Local<v8::Object> JsObj = v8::Object::New(Isolate); // Result
+	// algorithm outputs
+	TFltVV Z;
+	TFltV mu;
+	TFltV sigma;
+
+	TLAMisc::ZScore(JsMat->Mat, Z, Flag, Dim);
+	TLAMisc::Mean(JsMat->Mat, mu, Dim);
+	TLAMisc::Std(JsMat->Mat, sigma, Flag, Dim);
+
+	JsObj->Set(v8::String::NewFromUtf8(Isolate, "Z"), TNodeJsFltVV::New(Z));
+	JsObj->Set(v8::String::NewFromUtf8(Isolate, "mu"), TNodeJsVec< TFlt, TAuxFltV >::New(mu));
+	JsObj->Set(v8::String::NewFromUtf8(Isolate, "sigma"), TNodeJsVec< TFlt, TAuxFltV >::New(sigma));
+	Args.GetReturnValue().Set(JsObj);
 }
 
 ///////////////////////////////
@@ -65,6 +102,9 @@ void init(v8::Handle<v8::Object> exports) {
    // LA
    TNodeJsVec<TFlt, TAuxFltV>::Init(exports);
    TNodeJsVec<TInt, TAuxIntV>::Init(exports);
+   TNodeJsFltVV::Init(exports);
+   TNodeJsSpVec::Init(exports);
+   TNodeJsSpMat::Init(exports);
 }
 
-NODE_MODULE(stat, init) // Could this be a problem? Is stat allready reserved name??
+NODE_MODULE(stat, init)
