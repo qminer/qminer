02b4778ba80237387c57a096b6d214089b452a4e
Jan Rupnik <jan.rupnik@ijs.si>
2015-01-27 15:25:29 +0100

    npm update

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ var qminer = null;
  
 // Load the precompiled binary for windows.
 if(process.platform == "win32" && process.arch == "x64") {
-robot = require('./bin/winx64/robot');
+	qminer = require('./bin/winx64/qm.node');
 } else if(process.platform == "win32" && process.arch == "ia32") {
-robot = require('./bin/winx86/robot');
+	throw 'unsupported architecture';
 } else {
 	// Load the new built binary for other platforms.
-qminer = require('./build/Release/robot');
+	qminer = require('./build/Debug/qm.node');
 }
  
 module.exports = qminer;
