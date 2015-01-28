56f465863f622fe53094eaf31c9289190aa4ca95
Jan Rupnik <jan.rupnik@ijs.si>
2015-01-28 13:59:04 +0100

    npm updated

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 exports.analytics = require('bindings')('analytics');
 exports.fs = require('bindings')('fs');
 exports.ht = require('bindings')('ht');
-exports.la = require('bindings')('la'); //need script
-exports.qm = require('bindings')('qm'); //need script
+exports.la = require('./src/nodejs/scripts/la.js'); //need script
+exports.qm = require('./src/nodejs/scripts/qm.js');
 exports.snap = require('bindings')('snap');
 exports.stat = require('bindings')('stat');
 
