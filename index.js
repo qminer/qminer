5705396717e5e86c8b22180b57440d7790d4165a
Jan Rupnik <jan.rupnik@ijs.si>
2015-01-28 13:51:46 +0100

    npm index

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-exports.qm = require('bindings')('qm'); 
-exports.la = require('bindings')('la'); 
+exports.analytics = require('bindings')('analytics');
+exports.fs = require('bindings')('fs');
+exports.ht = require('bindings')('ht');
+exports.la = require('bindings')('la'); //need script
+exports.qm = require('bindings')('qm'); //need script
+exports.snap = require('bindings')('snap');
+exports.stat = require('bindings')('stat');
+
