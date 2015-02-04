2c94e5a02cc52c07686a86b176f3aa355f148494
Jan Rupnik <jan.rupnik@ijs.si>
2015-02-04 08:44:21 +0100

    version bump

--- test/nodejs/svm.js
+++ test/nodejs/svm.js
@@ -2,8 +2,6 @@ var assert = require('assert');
 var analytics = require('../../').analytics;
 var la = require('../../').la;
 
-console.log('hej');
-
 var vec = new la.Vector({vals:4});
 var mat = new la.Matrix({rows:2, cols:4});
 
@@ -14,16 +12,12 @@ var x = new la.Vector({vals:2});
 
 var SVC = new analytics.SVC({verbose:true});
 SVC.fit(mat,vec);
-SVC.save('fif.bin');
+SVC.save('svc.bin');
 
 var y1 = SVC.predict(x);
 
-console.log('fifa');
-
-
-
 var SVR = new analytics.SVR({verbose:true});
 SVR.fit(mat,vec);
-SVR.save('fif.bin');
+SVR.save('svr.bin');
 
 var y1 = SVR.predict(x);
