56c5eb97ad576f49b86cd1d7369e453a7c0cd3ed
Jan Rupnik <jan.rupnik@ijs.si>
2015-03-23 09:21:12 +0100

    minor

--- src/nodejs/la/la_structures_nodejs.h
+++ src/nodejs/la/la_structures_nodejs.h
@@ -104,7 +104,7 @@ public:
 * @classdesc Represents a dense matrix (2d array).
 * @class
 * @param {(module:la~matrixArg | Array<Array<number>> | module:la.Matrix)} [arg] - Constructor arguments. There are three ways of constructing:
-* <br>1. Parameter object module:la~matrixArg.
+* <br>1. Parameter object {@link module:la~matrixArg}.
 * <br>2. Nested array of matrix elements (row major). Example: [[1,2],[3,4]] has two rows, the first row is [1,2].
 * <br>3. A matrix (copy constructor).
 * @example
