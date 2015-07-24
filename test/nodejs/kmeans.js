de8eec662348d7bc9404d221b4c0b079946c3961
Viktor Jovanoski <viktor@carvic.si>
2015-07-24 14:15:15 +0200

    Small fixes after merge

--- test/nodejs/kmeans.js
+++ test/nodejs/kmeans.js
@@ -3,5 +3,9 @@ var qm = require('qminer');
 var X = qm.la.randn(2,1000);
 var kmeans = new qm.analytics.KMeans({iter : 100, k :10});
 kmeans.fit(X);
+var model = kmeans.getModel();
 
 idxv = kmeans.predict(X);
+
+// test output
+//console.log(model.C.toString());
