2e1bcb25610c4aa82eaec5f26df2afb73a7069de
Chris Pick <chris@chrispick.com>
2015-08-07 01:42:00 -0400

    Test ZipFL on an archive containing a single, empty file

--- test/cpp/test-zipfl.cpp
+++ test/cpp/test-zipfl.cpp
@@ -16,3 +16,18 @@ TEST(TZipIn, EmptyArchiveNew) {
   EXPECT_EQ(0,SIn->Len());
   EXPECT_TRUE(SIn->Eof());
 }
+
+// Test archive with single, empty file
+
+TEST(TZipIn, EmptyFileFLen) {
+  TStr FNm = "files/zero.7z";
+  EXPECT_EQ(0,TZipIn::GetFLen(FNm));
+}
+
+TEST(TZipIn, EmptyFileNew) {
+  TStr FNm = "files/zero.7z";
+  PSIn SIn = TZipIn::New(FNm);
+
+  EXPECT_EQ(0,SIn->Len());
+  EXPECT_TRUE(SIn->Eof());
+}
