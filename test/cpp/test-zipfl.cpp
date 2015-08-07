07b926f072a6357d48c4db0ff435581faf3a0d79
Chris Pick <chris@chrispick.com>
2015-08-07 03:14:51 -0400

    Test ZipFL on an archive with a single, file containing "1234567\n"

--- test/cpp/test-zipfl.cpp
+++ test/cpp/test-zipfl.cpp
@@ -31,3 +31,18 @@ TEST(TZipIn, EmptyFileNew) {
   EXPECT_EQ(0,SIn->Len());
   EXPECT_TRUE(SIn->Eof());
 }
+
+// Test archive with single file containing "1234567\n"
+
+TEST(TZipIn, EightFileFLen) {
+  TStr FNm = "files/eight.7z";
+  EXPECT_EQ(8,TZipIn::GetFLen(FNm));
+}
+
+TEST(TZipIn, EightFileNew) {
+  TStr FNm = "files/eight.7z";
+  PSIn SIn = TZipIn::New(FNm);
+
+  EXPECT_EQ(8,SIn->Len());
+  EXPECT_FALSE(SIn->Eof());
+}
