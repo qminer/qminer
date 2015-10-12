8dc6595c16def4bb5bc11468c386a605a09bc1f5
Luka Stopar <luka.stopar@ijs.si>
2015-10-12 13:20:48 +0200

    removed override

--- src/nodejs/analytics/analytics.h
+++ src/nodejs/analytics/analytics.h
@@ -1834,11 +1834,11 @@ public:
 	JsDeclareFunction(save);
 
 	// TMcCallback - callbacks
-	void OnStateChanged(const TIntFltPrV& StateIdHeightV) override;
-	void OnAnomaly(const TStr& AnomalyDesc) override;
-	void OnOutlier(const TFltV& FtrV) override;
+	void OnStateChanged(const TIntFltPrV& StateIdHeightV);
+	void OnAnomaly(const TStr& AnomalyDesc);
+	void OnOutlier(const TFltV& FtrV);
 	void OnPrediction(const uint64& RecTm, const int& CurrStateId, const int& TargetStateId,
-			const double& Prob, const TFltV& ProbV, const TFltV& TmV) override;
+			const double& Prob, const TFltV& ProbV, const TFltV& TmV);
 
 private:
 	void SetParams(const PJsonVal& ParamVal);
