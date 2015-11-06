2fe2c97a7acf391182664f7f79406ac2610fa955
Jan Rupnik <jan.rupnik@ijs.si>
2015-11-06 08:06:11 +0100

    testing 32bit

--- tools/publish32.sh
+++ tools/publish32.sh
@@ -1,12 +1,11 @@
-# prepare temporary doxyfile with current time stamp
-DOXYGEN_STIME=`date +"%Y-%m-%d %H:%M:%S"`
-sed "s/00000000/$DOXYGEN_STIME/" Doxyfile > Doxyfile-tmp
-# generate documentation
-doxygen Doxyfile-tmp
-# remove temporary doxyfile
-rm ./Doxyfile-tmp
-# remove old documentation if we have one
-rm -rf ../cppdoc
-rm -rf ../log-doxygen.txt
-# move new documentation to the root folder
-mv ./doc ../cppdoc
+./node_modules/node-pre-gyp/bin/node-pre-gyp clean
+NVER=`node -v`
+wget http://nodejs.org/dist/${NVER}/node-${NVER}-${platform}-x86.tar.gz
+tar xf node-${NVER}-${platform}-x86.tar.gz
+# enable 32 bit node
+export PATH=$(pwd)/node-${NVER}-${platform}-x86/bin:$PATH
+# install 32 bit compiler toolchain
+sudo apt-get -y install libx32gcc-4.8-dev libc6-dev-i386 gcc-4.8-multilib g++-4.8-multilib
+# try to compile in 32 bit mode
+CC=gcc-4.8 CXX=g++-4.8 npm install --build-from-source
+./node_modules/node-pre-gyp/bin/node-pre-gyp package unpublish publish
