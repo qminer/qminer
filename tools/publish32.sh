3508ffe47b4e2dc0124fc8bf090f8b118be3c4fc
Jan Rupnik <jan.rupnik@ijs.si>
2015-11-06 12:38:36 +0100

    tesi

--- tools/publish32.sh
+++ tools/publish32.sh
@@ -4,7 +4,11 @@ platform=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwx
 wget http://nodejs.org/dist/${NVER}/node-${NVER}-${platform}-x86.tar.gz
 tar xf node-${NVER}-${platform}-x86.tar.gz
 # enable 32 bit node
+echo current dir `pwd`
+ls
 export PATH=$(pwd)/node-${NVER}-${platform}-x86/bin:$PATH
+echo $PATH
+which node
 node -e "console.log(process.arch)"
 # install 32 bit compiler toolchain (libx32gcc-4.8-dev not found)
 sudo apt-get -y install libc6-dev-i386 gcc-4.8-multilib g++-4.8-multilib
