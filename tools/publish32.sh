9d16fca04e2a01bf1a8d2a53f1ecaebcf9b29034
Jan Rupnik <jan.rupnik@ijs.si>
2015-11-06 13:55:42 +0100

    test 32bit publish

--- tools/publish32.sh
+++ tools/publish32.sh
@@ -4,14 +4,9 @@ platform=$(uname -s | sed "y/ABCDEFGHIJKLMNOPQRSTUVWXYZ/abcdefghijklmnopqrstuvwx
 wget http://nodejs.org/dist/${NVER}/node-${NVER}-${platform}-x86.tar.gz
 tar xf node-${NVER}-${platform}-x86.tar.gz
 # enable 32 bit node
-echo current dir `pwd`
-ls
 export PATH=$(pwd)/node-${NVER}-${platform}-x86/bin:$PATH
-echo $PATH
-which node
-npm rebuild --ignore-scripts
 # install 32 bit compiler toolchain (libx32gcc-4.8-dev not found)
 sudo apt-get -y install libc6-dev-i386 gcc-4.8-multilib g++-4.8-multilib
 # try to compile in 32 bit mode
 CC=gcc-4.8 CXX=g++-4.8 npm install --build-from-source --debug
-./node_modules/node-pre-gyp/bin/node-pre-gyp package
+./node_modules/node-pre-gyp/bin/node-pre-gyp package unpublish publish
