dffd5dfa53481377b4ae9a24ae6258179fda9e79
Jan Rupnik <jan.rupnik@ijs.si>
2015-11-06 09:53:18 +0100

    testing 32bit

--- tools/publish32.sh
+++ tools/publish32.sh
@@ -5,8 +5,8 @@ wget http://nodejs.org/dist/${NVER}/node-${NVER}-${platform}-x86.tar.gz
 tar xf node-${NVER}-${platform}-x86.tar.gz
 # enable 32 bit node
 export PATH=$(pwd)/node-${NVER}-${platform}-x86/bin:$PATH
+node -e "console.log(process.arch)"
 # install 32 bit compiler toolchain
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
 sudo apt-get -y install libx32gcc-4.8-dev libc6-dev-i386 gcc-4.8-multilib g++-4.8-multilib
 # try to compile in 32 bit mode
 CC=gcc-4.8 CXX=g++-4.8 npm install --build-from-source
