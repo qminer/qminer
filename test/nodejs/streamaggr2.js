e1e038dfb24eb9f9226fb62ff173546db0a51b07
bergloman <bergloman@gmail.com>
2015-12-21 18:46:45 +0100

    added unit test for sfloat that is consumed by window-buffer stream aggregate

--- test/nodejs/streamaggr2.js
+++ test/nodejs/streamaggr2.js
@@ -101,6 +101,106 @@ describe("test histogram, slotted-histogram and histogram_diff aggregates", func
 			base.close();
 		}
 	});
+	it("histogram - sfloat", function () {
+		var qm = require('qminer');
+
+		// create a base with a simple store
+		// the store records results of clustering
+		var base = new qm.Base({
+			mode: "createClean",
+			schema: [
+			{
+				name: "Rpm",
+				fields: [
+					{ name: "ClusterId", type: "sfloat" },
+					{ name: "Time", type: "datetime" }
+				]
+			}]
+		});		
+		try {
+			var store = base.store('Rpm');
+
+			// create a new time series stream aggregator for the 'Rpm' store that takes the recorded cluster id
+			// and the timestamp from the 'Time' field. The size of the window is 2 hours.
+			var timeser = {
+				name: 'TimeSeries1',
+				type: 'timeSeriesWinBuf',
+				store: 'Rpm',
+				timestamp: 'Time',
+				value: 'ClusterId',
+				winsize: 2 * 60 * 60 * 1000 // 2 hours
+			};
+			var timeSeries1 = base.store("Rpm").addStreamAggr(timeser);
+
+			// add a histogram aggregator, that is connected with the 'TimeSeries1' aggregator
+			var aggrJson = {
+				name: 'Histogram1',
+				type: 'onlineHistogram',
+				store: 'Rpm',
+				inAggr: 'TimeSeries1',
+				lowerBound: 0,
+				upperBound: 5,
+				bins: 5,
+				addNegInf: false,
+				addPosInf: false
+			};
+			var hist1 = base.store("Rpm").addStreamAggr(aggrJson);
+
+			// add some values
+			store.push({ Time: '2015-06-10T00:13:30.0', ClusterId: 0 });
+			store.push({ Time: '2015-06-10T00:14:30.0', ClusterId: 0 });
+			store.push({ Time: '2015-06-10T00:15:31.0', ClusterId: 1 });
+			store.push({ Time: '2015-06-10T00:16:30.0', ClusterId: 0 });
+			store.push({ Time: '2015-06-10T00:17:30.0', ClusterId: 0 });
+			store.push({ Time: '2015-06-10T00:18:30.0', ClusterId: 0 });
+
+			var tmp1 = hist1.saveJson();
+			assert.equal(tmp1.counts.length, 5);
+			assert.equal(tmp1.counts[0], 5);
+			assert.equal(tmp1.counts[1], 1);
+			assert.equal(tmp1.counts[2], 0);
+			assert.equal(tmp1.counts[3], 0);
+			assert.equal(tmp1.counts[4], 0);
+			//console.log(JSON.stringify(tmp1));
+			
+			store.push({ Time: '2015-06-10T01:13:30.0', ClusterId: 0 });
+			store.push({ Time: '2015-06-10T02:14:30.0', ClusterId: 0 });
+			store.push({ Time: '2015-06-10T03:15:31.0', ClusterId: 1 });
+			store.push({ Time: '2015-06-10T04:16:30.0', ClusterId: 0 }); // this one will just leave the window
+			store.push({ Time: '2015-06-10T05:17:30.0', ClusterId: 2 });
+			store.push({ Time: '2015-06-10T06:18:30.0', ClusterId: 2 });
+
+			// just the last three
+			var tmp2 = hist1.saveJson();
+			assert.equal(tmp2.counts[0], 0);
+			assert.equal(tmp2.counts[1], 0);
+			assert.equal(tmp2.counts[2], 2);
+			assert.equal(tmp2.counts[3], 0);
+			assert.equal(tmp2.counts[4], 0);
+			//console.log(JSON.stringify(tmp2));
+
+			store.push({ Time: '2015-06-17T00:13:30.0', ClusterId: 2 });
+			store.push({ Time: '2015-06-17T00:14:30.0', ClusterId: 2 });
+			store.push({ Time: '2015-06-17T00:15:31.0', ClusterId: 3 });
+			store.push({ Time: '2015-06-17T00:16:30.0', ClusterId: 3 });
+			store.push({ Time: '2015-06-17T00:17:30.0', ClusterId: 4 });
+			store.push({ Time: '2015-06-17T00:18:30.0', ClusterId: 4 });
+
+			// this week
+			var tmp3 = hist1.saveJson();
+			assert.equal(tmp3.counts[0], 0);
+			assert.equal(tmp3.counts[1], 0);
+			assert.equal(tmp3.counts[2], 2);
+			assert.equal(tmp3.counts[3], 2);
+			assert.equal(tmp3.counts[4], 2);
+			//console.log(JSON.stringify(tmp3));
+
+			// show distribution for expected values
+			//console.log(hist1);
+		} finally {
+			base.close();
+		}
+	});
 	it("histogram reset", function () {
 	    var qm = require('qminer');
 
