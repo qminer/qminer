7675c7fe900a9821df8962264c29fc21f47997ef
Blaz Fortuna <blaz@blazfortuna.com>
2016-01-19 14:17:31 +0100

    Removed excessive output in test.

--- test/nodejs/jointest.js
+++ test/nodejs/jointest.js
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2015, Jozef Stefan Institute, Quintelligence d.o.o. and contributors
  * All rights reserved.
- * 
+ *
  * This source code is licensed under the FreeBSD license found in the
  * LICENSE file in the root directory of this source tree.
  */
@@ -40,7 +40,7 @@ describe('Index-Join Test', function () {
         assert(base.store('People')[id1].friends[0].$fq == 7);
         assert(base.store('People')[id1].friends[1].name == "Jim");
         assert(base.store('People')[id1].friends[1].$fq == 8);
-        
+
         assert(base.store('People')[id2].friends.length == 2);
         assert(base.store('People')[id2].friends[0].name == "John");
         assert(base.store('People')[id2].friends[0].$fq == 9);
@@ -48,20 +48,19 @@ describe('Index-Join Test', function () {
         assert(base.store('People')[id2].friends[1].$fq == 3);
 
         // check that filterByField handles index-joins
-        
+
         var rs_all = base.store('People')
             .allRecords
             .filterByField("friends", id2, id2); // return all that have Mary as friend => John, Mary
-            
-        console.log(rs_all.length);
+
+        //console.log(rs_all.length);
         assert(rs_all.length == 2);
         assert(rs_all[0].$id == id1);
         assert(rs_all[0].name == "John");
         assert(rs_all[1].$id == id2);
         assert(rs_all[1].name == "Mary");
-            
 
-        base.close();    
+        base.close();
     })
 });
 
@@ -78,7 +77,7 @@ function CreateBase(type, type2, sloc) {
                 joins: [{ name: 'parent', type: 'field', store: 'People', storage: type + "-" + type2, storage_location: sloc || "memory" }]
             }
         ]
-    });    
+    });
     return base;
 }
 
@@ -89,22 +88,22 @@ function FillAndCheck(base, rec_id_type, freq_type) {
 
     var rec1 = base.store('People')[id1];
     var rec2 = base.store('People')[id2];
-    
+
     rec1.$addJoin('parent', id2, 7);
     rec2.$addJoin('parent', id3, 8);
-    
+
     // reload data, just in case
     rec1 = base.store('People')[id1];
     rec2 = base.store('People')[id2];
-    
+
     assert(rec1.parent.name == "Mary");
     assert(rec1.parent.$fq == (freq_type == "" ? 1 : 7));
-    
+
     assert(rec2.parent.name == "Jim");
     assert(rec2.parent.$fq == (freq_type == "" ? 1 : 8));
 
     // perform filterByField
-    
+
     var rs_all = base.store('People')
         .allRecords
         .filterByField("parent", id2, id2);
@@ -112,7 +111,7 @@ function FillAndCheck(base, rec_id_type, freq_type) {
     assert(rs_all[0].$id == id1);
     assert(rs_all[0].name == "John");
 
-    base.close();    
+    base.close();
 }
 
 // field joins
@@ -127,13 +126,13 @@ describe('Different join-field-type tests', function () {
             for (var i = 0 ; i < rec_id_types.length; i++) {
                 for (var j = 0 ; j < freq_types.length; j++) {
                     var rec_id_type = rec_id_types[i];
-                    var freq_type = freq_types[j]; 
-                    
-                    console.log(rec_id_type, "-", freq_type, " ", sloc);
+                    var freq_type = freq_types[j];
+
+                    //console.log(rec_id_type, "-", freq_type, " ", sloc);
                     var base = CreateBase(rec_id_type, freq_type, sloc);
                     FillAndCheck(base, rec_id_type, freq_type);
-                }    
-            }            
+                }
+            }
         }
     })
 });
