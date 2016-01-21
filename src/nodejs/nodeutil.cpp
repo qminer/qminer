2eea972ea1d7d4e91e312341d0bb2bd94df5b708
Luka Stopar <luka.stopar@ijs.si>
2016-01-21 16:24:11 +0100

    testing ...

--- src/nodejs/nodeutil.cpp
+++ src/nodejs/nodeutil.cpp
@@ -794,7 +794,7 @@ void TNodeTask::AfterRun() {
 		// TODO remove
 		PJsonVal ResVal = TNodeJsUtil::GetObjJson(Result);
 		printf("In after run: %s\n", ResVal->GetStr().CStr());
-		//=========================
+		//============================
 
 		const int ArgC = 2;
 		v8::Handle<v8::Value> ArgV[ArgC] = { v8::Undefined(Isolate), Result };
