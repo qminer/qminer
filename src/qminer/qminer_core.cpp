023ce5b59cc3f5c5ee3131834a70d1ebb12e8897
Blaz Fortuna <blaz@blazfortuna.com>
2016-08-09 15:38:39 +0200

    Cleanup of syntax

--- src/qminer/qminer_core.cpp
+++ src/qminer/qminer_core.cpp
@@ -1495,8 +1495,7 @@ TRec::TRec(const TWPt<TStore>& _Store, const PJsonVal& JsonVal) :
         case oftTMem: {
             QmAssertR(FieldVal->IsStr(), "Provided JSon data field " + FieldDesc.GetFieldNm() + " is not a number or a string that represents DateTime.");
             // TODO do we support anything else? probably not on this level...
-            TMem Mem;
-            TStr::Base64Decode(FieldVal->GetStr(), Mem);
+            TMem Mem; TStr::Base64Decode(FieldVal->GetStr(), Mem);
             SetFieldTMem(FieldId, Mem);
             break;
         }
@@ -1585,6 +1584,7 @@ int TRec::GetFieldInt(const int& FieldId) const {
     }
     throw FieldError(FieldId, "Int");
 }
+
 int16 TRec::GetFieldInt16(const int& FieldId) const {
     if (IsByRef()) {
         return Store->GetFieldInt16(RecId, FieldId);
@@ -1595,6 +1595,7 @@ int16 TRec::GetFieldInt16(const int& FieldId) const {
     }
     throw FieldError(FieldId, "Int16");
 }
+
 int64 TRec::GetFieldInt64(const int& FieldId) const {
     if (IsByRef()) {
         return Store->GetFieldInt64(RecId, FieldId);
@@ -1605,6 +1606,7 @@ int64 TRec::GetFieldInt64(const int& FieldId) const {
     }
     throw FieldError(FieldId, "Int64");
 }
+
 uchar TRec::GetFieldByte(const int& FieldId) const {
     if (IsByRef()) {
         return Store->GetFieldByte(RecId, FieldId);
@@ -1638,6 +1640,7 @@ uint TRec::GetFieldUInt(const int& FieldId) const {
     }
     throw FieldError(FieldId, "UInt");
 }
+
 uint16 TRec::GetFieldUInt16(const int& FieldId) const {
     if (IsByRef()) {
         return (uint16)Store->GetFieldUInt64(RecId, FieldId);
@@ -1648,6 +1651,7 @@ uint16 TRec::GetFieldUInt16(const int& FieldId) const {
     }
     throw FieldError(FieldId, "UInt16");
 }
+
 uint64 TRec::GetFieldUInt64(const int& FieldId) const {
     if (IsByRef()) {
         return Store->GetFieldUInt64(RecId, FieldId);
@@ -1886,7 +1890,8 @@ PJsonVal TRec::GetFieldJson(const int& FieldId) const {
         return TJsonVal::NewArr(FieldFltV);
     } else if (Desc.IsTm()) {
         TTm FieldTm; GetFieldTm(FieldId, FieldTm);
-        if (FieldTm.IsDef()) { return TJsonVal::NewStr(FieldTm.GetWebLogDateTimeStr(true, "T", true)); } else { return TJsonVal::NewNull(); }
+        if (FieldTm.IsDef()) { return TJsonVal::NewStr(FieldTm.GetWebLogDateTimeStr(true, "T", true)); }
+        else { return TJsonVal::NewNull(); }
     } else if (Desc.IsNumSpV()) {
         TIntFltKdV FieldIntFltKdV; GetFieldNumSpV(FieldId, FieldIntFltKdV);
         return TJsonVal::NewStr(TStrUtil::GetStr(FieldIntFltKdV));
@@ -2166,6 +2171,7 @@ PRecSet TRec::ToRecSet() const {
     QmAssertR(IsByRef(), "Cannot transform record passed by value to a set!");
     return IsDef() ? TRecSet::New(Store, RecId) : TRecSet::New(Store);
 }
+
 /// Returns record-id of given field join
 uint64 TRec::GetFieldJoinRecId(const int& JoinId) const {
     QmAssertR(Store->IsJoinId(JoinId), "Invalid JoinId");
@@ -7286,7 +7292,8 @@ bool TBase::RestoreJSonDump(const TStr& DumpDir) {
                 TStrV PartV; Line.SplitOnAllCh('|', PartV, false);
                 AssertR(PartV.Len() == 3, TStr::Fmt("The line with json data did not contain three parts when split with |. Store Name: %s, Line val: %s", StoreNm.CStr(), Line.CStr()));
                 const uint64 OldRecId = PartV[0].GetUInt64();
-                const uint64 NewRecId = OldToNewIdH.GetDat(OldRecId);   // map old rec ids to new rec ids
+                // map old rec ids to new rec ids
+                const uint64 NewRecId = OldToNewIdH.GetDat(OldRecId);
                 if (!Store->IsRecId(NewRecId)) {
                     TQm::TEnv::Logger->OnStatusFmt("ERROR: Failed to create join for missing record %I64U in store %s.", NewRecId, StoreNm.CStr());
                     continue;
@@ -7308,7 +7315,9 @@ bool TBase::RestoreJSonDump(const TStr& DumpDir) {
                 for (int N = 0; N < Joins; N++) {
                     TStr JoinRecIdStr, JoinFqStr; JoinV[N].SplitOnCh(JoinRecIdStr, ',', JoinFqStr);
                     const uint64 OldJoinRecId = JoinRecIdStr.GetUInt64();
-                    const uint64 NewJoinRecId = JoinOldToNewIdH.GetDat(OldJoinRecId);       // if some articles or other data was deleted from the index then old and new ids could be different. in most cases it should be the same
+                    // Ff some articles or other data was deleted from the index then old and
+                    // new ids could be different. In most cases it should be the same.
+                    const uint64 NewJoinRecId = JoinOldToNewIdH.GetDat(OldJoinRecId);
                     const int JoinFq = JoinFqStr.GetInt();
                     Store->AddJoin(NewJoinId, NewRecId, NewJoinRecId, JoinFq);
                 }
@@ -7461,8 +7470,7 @@ void TBase::SaveBaseConf(const TStr& FPath) const {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-
-/// Export TBlobBsStats object to JSON
+// Export TBlobBsStats object to JSON
 PJsonVal BlobBsStatsToJson(const TBlobBsStats& stats) {
     PJsonVal res = TJsonVal::NewObj();
     res->AddToObj("alloc_count", stats.AllocCount);
@@ -7481,7 +7489,6 @@ PJsonVal BlobBsStatsToJson(const TBlobBsStats& stats) {
     res->AddToObj("size_changes", stats.SizeChngs);
     return res;
 }
-
 
 /// Export TGixStats object to JSON
 PJsonVal GixStatsToJson(const TGixStats& stats) {
