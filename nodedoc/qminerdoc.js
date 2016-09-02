b6836cee3fced939936371e8b519a2eb48437f39
Blaz Fortuna <blaz@blazfortuna.com>
2016-09-02 16:12:30 +0200

    Documentation update

--- nodedoc/qminerdoc.js
+++ nodedoc/qminerdoc.js
@@ -175,7 +175,9 @@
 * @property {string} name - The name of the join.
 * @property {string} type - Join types. Possible options:
 * <br>1. `'field'` - Points to zero or one record and is implemented as an additional hidden field of type `uint64`, which can hold the ID of the record it links to. Accessing the records join returns a record.
-* <br>2. `'index'` - Point to any number of records and is implemented using the inverted index, where for each record a list (vector) of linked records is kept. Accessing the records join returns a record set.
+* <br>2. `'index'` - Point to any number of records and is implemented using the inverted index, where for each record a list (vector) of linked records is kept. Accessing the records join returns a record set. 
+* <b>Important:</b> The records given to this join field must be in an array.
+*
 * @property {string} store - The store name from which the linked records are.
 * @example
 * var qm = require('qminer');
@@ -200,17 +202,22 @@
 * // Adds a movie, automatically adds 'Jim Jarmusch' to People, sets the 'director' join (field join)
 * // and automatically updates the index join 'directed', since it's an inverse join of 'director'
 * base.store('Movies').push({ title: 'Broken Flowers', director: { name: 'Jim Jarmusch' } });
+*
 * // Adds a movie, sets the 'director' join, updates the index join of 'Jim Jarmusch'
 * base.store('Movies').push({ title: 'Coffee and Cigarettes', director: { name: 'Jim Jarmusch' } });
 * // Adds movie, automatically adds 'Lars von Trier' to People, sets the 'director' join
 * // and 'directed' inverse join (automatically)
 * base.store('Movies').push({ title: 'Dogville', director: { name: 'Lars von Trier' } });
 *
+* // Adds a person, sets the 'directed' join with multiple movies ('directed' is of type 'index', movies must be given in an array)
+* base.store('People').push({ name: 'Christopher Nolan', directed: [{ title: 'Inception' }, { title: 'Interstellar' }] });
+*
 * var movie = base.store('Movies')[0]; // get the first movie (Broken Flowers)
 * // Each movie has a property corresponding to the join name: 'director'. 
 * // Accessing the property returns a {@link module:qm.Record} from the store People.
 * var person = movie.director; // get the director
 * var personName = person.name; // get person's name ('Jim Jarmusch')
+*
 * // Each person has a property corresponding to the join name: 'directed'. 
 * // Accessing the property returns a {@link module:qm.RecSet} from the store People.
 * var movies = person.directed; // get all the movies the person directed.
@@ -1572,7 +1579,7 @@
     */
  exports.Record.prototype.store = Object.create('qminer').Store.prototype;
 /**
- * Vector of records by value
+ * Vector of records by value.
  * @class
  * @param {module:fs.FIn} [arg] - Load vector from input stream.
  * @classdesc Vector storing records defined by value. Vector can be serialized and
@@ -1595,11 +1602,11 @@
  * // Create record vector
  * var recordVector = new qm.RecordVector(base);
  * // Add some records to the vector
- * recordVector.push(store("Philosophers").newRecord({ Name: "Plato", Era: "Ancient philosophy" }));
- * recordVector.push(store("Philosophers").newRecord({ Name: "Immanuel Kant", Era: "18th-century philosophy" }));
- * recordVector.push(store("Philosophers").newRecord({ Name: "Emmanuel Levinas", Era: "20th-century philosophy" }));
- * recordVector.push(store("Philosophers").newRecord({ Name: "Rene Descartes", Era: "17th-century philosophy" }));
- * recordVector.push(store("Philosophers").newRecord({ Name: "Confucius", Era: "Ancient philosophy" }));
+ * recordVector.push(base.store("Philosophers").newRecord({ Name: "Plato", Era: "Ancient philosophy" }));
+ * recordVector.push(base.store("Philosophers").newRecord({ Name: "Immanuel Kant", Era: "18th-century philosophy" }));
+ * recordVector.push(base.store("Philosophers").newRecord({ Name: "Emmanuel Levinas", Era: "20th-century philosophy" }));
+ * recordVector.push(base.store("Philosophers").newRecord({ Name: "Rene Descartes", Era: "17th-century philosophy" }));
+ * recordVector.push(base.store("Philosophers").newRecord({ Name: "Confucius", Era: "Ancient philosophy" }));
  * // Iterate over all records
  * for (var i = 0; i < recordVector.length; i++) {
  *    var rec = recordVector[i];
@@ -1629,7 +1636,7 @@
      * // Create record vector
      * var recordVector = new qm.RecordVector(base);
      * // Add some records to the vector
-     * recordVector.push(store("Philosophers").newRecord({ Name: "Plato", Era: "Ancient philosophy" }));
+     * recordVector.push(base.store("Philosophers").newRecord({ Name: "Plato", Era: "Ancient philosophy" }));
      * base.close();
      */
  exports.RecordVector.prototype.push = function (rec) {};
@@ -1665,7 +1672,7 @@
      * // Create record vector
      * var recordVector = new qm.RecordVector(base);
      * // Add some records to the vector
-     * recordVector.push(store("Philosophers").newRecord({ Name: "Plato", Era: "Ancient philosophy" }));
+     * recordVector.push(base.store("Philosophers").newRecord({ Name: "Plato", Era: "Ancient philosophy" }));
      * // save to disk
      * var fout = fs.openWrite('record_vector.bin');
      * recordVector.save(fout).close();
@@ -2668,7 +2675,7 @@
 *       ]
 *    }]
 * });
-* // create a feature space containing the categorical extractor, where the it's values
+* // create a feature space containing the categorical extractor, where it's values
 * // are taken from the field "StudyGroup": "A", "B", "C" and "D"
 * var ftr = new qm.FeatureSpace(base, { type: "categorical", source: "Class", field: "StudyGroup" });
 * base.close();
@@ -3191,7 +3198,7 @@
     * // create a feature matrix out of the records of the store by using the feature space
     * // returns a sparse matrix equal to
     * // 1  0  0  1
-    * // 0  1  0  1
+    * // 0  1  1  1
     * // 0  0  1  0
     * // 1  1  0  0
     * var matrix = ftr.extractMatrix(base.store("Class").allRecords);
