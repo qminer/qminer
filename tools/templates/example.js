7b6d213e72f07a9482ebb81c91a3dca30afdf062
Jan Rupnik <jan.rupnik@ijs.si>
2017-09-04 15:28:24 +0200

    tabs

--- tools/templates/example.js
+++ tools/templates/example.js
@@ -1,10 +1,10 @@
 describe("${describe}', number ${number}", function () {
-	it("should make test number ${number}", function (done) {
-		try {
-			${content}
-			done();
-		} catch (e) {
-			done(e);
-		}
-	});
+    it("should make test number ${number}", function (done) {
+        try {
+            ${content}
+            done();
+        } catch (e) {
+            done(e);
+        }
+    });
 });
