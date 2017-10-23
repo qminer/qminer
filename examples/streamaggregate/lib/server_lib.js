6d3b358875acfe2cc434513318fb21f3eecd1d96
ErikNovak <novak.erik@gmail.com>
2017-10-23 15:11:13 +0200

    Changes addressing github comments

--- examples/streamaggregate/lib/server_lib.js
+++ examples/streamaggregate/lib/server_lib.js
@@ -16,30 +16,30 @@ setInterval(qmBackend.pushGaussianMerger, 1000, qmBackend);
 
 // make server function
 function makeServer() {
-	var pathDir = path.resolve(__dirname + `/../`);
-	// adding the css files (or static files) to the server
-	app.use(express.static(pathDir + '/public'));
+    var pathDir = path.resolve(__dirname + `/../`);
+    // adding the css files (or static files) to the server
+    app.use(express.static(pathDir + '/public'));
 
-	// when the client calls the server, it calls the dataSystemStats() and the example.html
-	app.get('/', function (req, res) {
-	    res.sendFile(pathDir + '/html/index.html');
-	});
+    // when the client calls the server, it calls the dataSystemStats() and the example.html
+    app.get('/', function (req, res) {
+        res.sendFile(pathDir + '/html/index.html');
+    });
     app.get('/moving_average.html', function (req, res) {
-	    res.sendFile(pathDir + '/html/moving_average.html');
-	});
-	app.get('/resampler.html', function (req, res) {
-	    res.sendFile(pathDir + '/html/resampler.html');
-	});
-	app.get('/merger.html', function (req, res) {
-	    res.sendFile(pathDir + '/html/merger.html');
-	});
+        res.sendFile(pathDir + '/html/moving_average.html');
+    });
+    app.get('/resampler.html', function (req, res) {
+        res.sendFile(pathDir + '/html/resampler.html');
+    });
+    app.get('/merger.html', function (req, res) {
+        res.sendFile(pathDir + '/html/merger.html');
+    });
 
-	// creates the server listener, so that we know that it's connected
-	var server = http.listen(3000, function () {
-		var port = server.address().port;
-		console.log('Server started at port %s', port);
-	});
-	return server;
+    // creates the server listener, so that we know that it's connected
+    var server = http.listen(3000, function () {
+        var port = server.address().port;
+        console.log('Server started at port %s', port);
+    });
+    return server;
 }
 
 qmBackend.createIOStream(io);
--- examples/streamaggregate/test/streamaggregate.js
+++ examples/streamaggregate/test/streamaggregate.js
@@ -7,24 +7,24 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 describe('Streamaggregate example test', function () {
-	this.timeout(10000);
-	var qmBackend;
-	before(function () {
-		qmBackend = new qminerBackend();
-	});
+    this.timeout(10000);
+    var qmBackend;
+    before(function () {
+        qmBackend = new qminerBackend();
+    });
 
-	// close server after each test
+    // close server after each test
     after(function () {
-    	qmBackend.base.close();
+        qmBackend.base.close();
     });
 
     // test server
-	it('shouldn\'t crash', function () {
-		assert.doesNotThrow(function () {
-			qmBackend.pushBrownianStats(qmBackend);
-			qmBackend.pushBrownianResampler(qmBackend);
-			qmBackend.pushGaussianMerger(qmBackend);
-			qmBackend.createIOStream(io);
-		});
-	});
+    it('shouldn\'t crash', function () {
+        assert.doesNotThrow(function () {
+            qmBackend.pushBrownianStats(qmBackend);
+            qmBackend.pushBrownianResampler(qmBackend);
+            qmBackend.pushGaussianMerger(qmBackend);
+            qmBackend.createIOStream(io);
+        });
+    });
 });
