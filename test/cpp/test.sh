688ffbc1c2b0207c1b70215d3d1835a4d88bb1bc
bergloman <bergloman@users.noreply.github.com>
2018-04-03 08:20:21 +0200

    Update test.sh

--- test/cpp/test.sh
+++ test/cpp/test.sh
@@ -2,5 +2,11 @@
 # a single fail results in global fail
 set -e
 
-ls -l build/Release
-build/Release/qminer-test
+if [ -d "build/Release" ]; then
+  ls -l build/Release
+  build/Release/qminer-test
+fi
+if [ -d "build/Debug" ]; then
+  ls -l build/Debug
+  build/Debug/qminer-test
+fi
