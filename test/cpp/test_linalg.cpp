ba373f5485c5ea87fb060f7d669994a6c43118bb
Carolina <carolina.parvu@gmail.com>
2025-11-09 16:29:14 +0100

    Undo extra printfs in test.

--- test/cpp/test_linalg.cpp
+++ test/cpp/test_linalg.cpp
@@ -41,72 +41,36 @@ TEST(TLinAlgOperatorsSubtract) {
 
 // DISABLED: Test hangs in destructor on macOS with C++17 - investigating memory management issue
 #if 0
-TEST(TLinAlgOperatorsMultiply) {
-    printf("[DEBUG] Starting TLinAlgOperatorsMultiply test\n"); fflush(stdout);
-
+TEST(TLinAlg, OperatorsMultiply) {
     // MATRICES
-    printf("[DEBUG] Creating matrices\n"); fflush(stdout);
-    TIntPr Dim1{ 3, 4 };
-    TIntPr Dim2{ 4, 2 };
-    TIntPr BadDim{ 7, 4 };
+    TIntPr Dim1 {3, 4};
+    TIntPr Dim2 {4, 2};
+    TIntPr BadDim {7, 4};
 
-    TFltVV X{ Dim1.Val1, Dim1.Val2 };    InitFltVV(X);
-    TFltVV Y{ Dim2.Val1, Dim2.Val2 };    InitFltVV(Y);
-    const double k{ 3 };
-    printf("[DEBUG] Matrices initialized\n"); fflush(stdout);
+    TFltVV X {Dim1.Val1, Dim1.Val2};    InitFltVV(X);
+    TFltVV Y {Dim2.Val1, Dim2.Val2};    InitFltVV(Y);
+    const double k {3};
 
-    printf("[DEBUG] Computing X * Y (operator overload)\n"); fflush(stdout);
     const TFltVV Z1 = X * Y;
-    printf("[DEBUG] Computing expected result with TLinAlg::Multiply\n"); fflush(stdout);
     TFltVV ExpectedZ1;  TLinAlg::Multiply(X, Y, ExpectedZ1);
-    printf("[DEBUG] Matrix multiplications complete\n"); fflush(stdout);
 
-    printf("[DEBUG] Computing Z1 - ExpectedZ1\n"); fflush(stdout);
-    TFltVV diff1 = Z1 - ExpectedZ1;
-    printf("[DEBUG] Computing FrobNorm\n"); fflush(stdout);
-    double norm1 = TLinAlg::FrobNorm(diff1);
-    printf("[DEBUG] FrobNorm = %f, asserting near 0\n", norm1); fflush(stdout);
-    ASSERT_NEAR(norm1, 0, Tol);
-    printf("[DEBUG] First assertion passed\n"); fflush(stdout);
+    ASSERT_NEAR(TLinAlg::FrobNorm(Z1 - ExpectedZ1), 0, Tol);
 
-    printf("[DEBUG] Testing scalar multiplication X*k\n"); fflush(stdout);
     const TFltVV Z2 = X*k;
     TFltVV ExpectedZ2;  TLinAlg::MultiplyScalar(k, X, ExpectedZ2);
-    printf("[DEBUG] Scalar multiplication complete\n"); fflush(stdout);
 
-    printf("[DEBUG] Computing Z2 - ExpectedZ2\n"); fflush(stdout);
-    TFltVV diff2 = Z2 - ExpectedZ2;
-    printf("[DEBUG] Computing FrobNorm for scalar test\n"); fflush(stdout);
-    double norm2 = TLinAlg::FrobNorm(diff2);
-    printf("[DEBUG] FrobNorm = %f, asserting near 0\n", norm2); fflush(stdout);
-    ASSERT_NEAR(norm2, 0, Tol);
-    printf("[DEBUG] Second assertion passed\n"); fflush(stdout);
-
-    printf("[DEBUG] Testing bad dimension exception\n"); fflush(stdout);
+    ASSERT_NEAR(TLinAlg::FrobNorm(Z2 - ExpectedZ2), 0, Tol);
     ASSERT_ANY_THROW(X*TFltVV(BadDim.Val1, BadDim.Val2));
-    printf("[DEBUG] Exception test passed\n"); fflush(stdout);
 
 
     // VECTORS
-    printf("[DEBUG] Starting vector tests\n"); fflush(stdout);
-    const int VecDim{ 5 };
-    TFltV Vec1{ VecDim, VecDim };    InitFltV(Vec1);
-    TFltV Vec2{ VecDim, VecDim };    InitFltV(Vec2);
-    TFltV BadVec{ VecDim + 1, VecDim + 1 };
-    printf("[DEBUG] Vectors initialized\n"); fflush(stdout);
+    const int VecDim {5};
+    TFltV Vec1 {VecDim, VecDim};    InitFltV(Vec1);
+    TFltV Vec2 {VecDim, VecDim};    InitFltV(Vec2);
+    TFltV BadVec {VecDim+1, VecDim+1};
 
-    printf("[DEBUG] Computing Vec1 * Vec2 dot product\n"); fflush(stdout);
-    double dotProduct = Vec1 * Vec2;
-    double expectedDot = TLinAlg::DotProduct(Vec1, Vec2);
-    printf("[DEBUG] Dot products: %f vs %f\n", dotProduct, expectedDot); fflush(stdout);
-    ASSERT_NEAR(dotProduct, expectedDot, Tol);
-    printf("[DEBUG] Vector dot product assertion passed\n"); fflush(stdout);
-
-    printf("[DEBUG] Testing vector bad dimension exception\n"); fflush(stdout);
+    ASSERT_NEAR(Vec1 * Vec2, TLinAlg::DotProduct(Vec1, Vec2), Tol);
     ASSERT_ANY_THROW(Vec1 * BadVec);
-    printf("[DEBUG] Vector exception test passed\n"); fflush(stdout);
-
-    printf("[DEBUG] TLinAlgOperatorsMultiply test completed successfully\n"); fflush(stdout);
 }
 #endif
 
